2bdb5ac5dfd2f53d52cde0fb83bf85ac1d0b52b2
Randy Strobel <randy@marxent.com>
2019-02-27 17:37:37 -0500

    Added ShowSummary and updated DomainConstants.

--- src/main/java/com/rjs/myshows/domain/DomainConstants.java
+++ src/main/java/com/rjs/myshows/domain/DomainConstants.java
@@ -2,7 +2,9 @@ package com.rjs.myshows.domain;
 
 public class DomainConstants {
 	public static final String VALUE_DELIMITER = ", ";
-	public static final String[] SHOW_TYPES = {"Movie", "TV"};
+	public static final String MOVIE_TYPE = "Movie";
+	public static final String TV_TYPE = "TV";
+	public static final String[] SHOW_TYPES = {MOVIE_TYPE, TV_TYPE};
 	public static final String[] MOVIE_RATINGS = {"G", "PG", "PG-13", "R", "NC-17", "N/R"};
 	public static final String[] MOVIE_CONTENTS = {"Language", "Violence", "Substance Abuse", "Nudity", "Sexual Content"};
 	public static final String[] TV_RATINGS = {"TV-Y", "TV-Y7", "TV-G", "TV-PG", "TV-14", "TV-MA", "N/R"};
---
+++ src/main/java/com/rjs/myshows/domain/dto/ShowSummary.java
@@ -0,0 +1,9 @@
+package com.rjs.myshows.domain.dto;
+
+public class ShowSummary {
+	public int id = -1;
+	public String title = "";
+	public String overview = "";
+	public String posterPath = "";
+	public String releaseDate = "";
+}
